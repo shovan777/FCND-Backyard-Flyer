562be1a4f449abfffa9e5e8ce4918f1c8e718936
shovan777 <raj.shrestha778@gmail.com>
2018-12-28 17:07:01 +0545

    cost done

---
+++ backyard_flyer_dronekit.py
@@ -0,0 +1,30 @@
+from dronekit import connect, Vehicle
+
+class States(Enum):
+    MANUAL = 0
+    ARMING = 1
+    TAKEOFF = 2
+    WAYPOINT = 3
+    LANDING = 4
+    DISARMING = 5
+
+class BackyardFlyer(Vehicle):
+    def __init__(self, *args):
+        super(BackyardFlyer, self).__init__(*args)
+        self.target_position = np.array([0.0, 0.0, 0.0])
+        self.all_waypoints = []
+        self.in_mission = True
+        self.check_state = {}
+
+        # initial state
+        self._flightmode = States
+
+
+
+
+
+
+
+
+
+vehicle = connect(connection_string, wait_ready=True, vehicle_class=BackyardFlyer)
